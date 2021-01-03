375adfcd37695957103c313f55dd524b86bd05e3
keshabDas <keshab.das@vivant.me>
2021-01-03 21:35:46 +0530

    changed the themeprovider HOC location

--- src/App.js
+++ src/App.js
@@ -9,35 +9,15 @@ import './App.css';
 
 function App() {
   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  let currentMode = parseInt(window.localStorage.getItem('darkMode'), 10);
-  const { changeTheme } = useContext(GlobalContext);
-
-  const [darkState, setDarkState] = useState(currentMode);
-  const palletType = darkState ? "dark" : "light";
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: palletType,
-    }
-  });
-
-  const handleThemeChange = () => {
-    setDarkState(!darkState);
-  };
-
-  useEffect(() => {
-    document.body.classList.toggle('darkMode', darkState);
-  }, [darkState])
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Provider>
-        <div>
-          <Container maxWidth={"lg"} >
-            <Mainview isMobile={isMobile} handleThemeChange={handleThemeChange} />
-          </Container>
-        </div>
-      </Provider>
-    </ThemeProvider>
+    <Provider>
+      <div>
+        <Container maxWidth={"lg"} >
+          <Mainview isMobile={isMobile} />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import TodoForm from '../components/TodoForm';
 import Header from '../components/Header';
 import WebView from './WebView';
@@ -8,12 +8,31 @@ import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import CustomSnackbar from '../components/CustomSnackbar';
 import CustomDialog from '../components/CustomDialog';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { GlobalContext } from '../context/Provider';
 
 function Mainview(props) {
+    const { isDarkModeOn } = useContext(GlobalContext);
+    const [darkState, setDarkState] = useState(isDarkModeOn);
+
+    const darkTheme = createMuiTheme({
+      palette: {
+        type: darkState ? "dark" : "light",
+      }
+    });
+
+    const handleThemeChange = () => {
+      setDarkState(!darkState);
+    };
+
+    useEffect(() => {
+      document.body.classList.toggle('darkMode', darkState);
+    }, [darkState]);
+
     return (
-        <div>
+        <ThemeProvider theme={darkTheme}>
             <div id="back-to-top-anchor"></div>
-            <Header text="Task Tracker" {...props} />
+            <Header text="Task Tracker" {...props} handleThemeChange={handleThemeChange} />
             <br />
             <TodoForm />
             {props.isMobile ? <MobileView isMobile={props.isMobile}  /> : <WebView isMobile={props.isMobile} />}
@@ -24,7 +43,7 @@ function Mainview(props) {
             </ScrollTop>
             <CustomSnackbar />
             <CustomDialog />
-        </div>
+        </ThemeProvider >
     )
 }
 
